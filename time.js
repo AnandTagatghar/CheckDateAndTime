6e940bc7a5b1444829176802d93dadb9da39d88f
AnandTagatgar <79243140+AnandTagatgar@users.noreply.github.com>
2021-03-19 21:37:42 +0530

    Update time.js

--- time.js
+++ time.js
@@ -7,7 +7,7 @@ function clickHandler(){
   var obj = new Date();
   var time = obj.toLocaleString("en-US", {timeZone:two});
   if(two == null){
-    document.getElementById('str3') = "give the currrect location.";
+    document.getElementById('str3').innerHTML = "give the currrect location.";
   }
   else{
     document.getElementById('str3').innerHTML = time;
